d9eb3378d85c7f0a5346cac50fff5448432a7780
danielnachun <daniel.nachun@gmail.com>
2022-11-14 15:03:08 -0800

    Fix script

--- .setup_env.sh
+++ .setup_env.sh
@@ -1,4 +1,2 @@
-#!/bin/bash
-
 curl -fLo $HOME/.local/bin/yadm https://github.com/TheLocehiliosan/yadm/raw/master/yadm && chmod a+x $HOME/.local/bin/yadm
 yadm clone --bootstrap https://github.com/danielnachun/yadm
