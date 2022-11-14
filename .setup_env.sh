8af0be903fecf2ffc96cd2912e689f906981fb3a
danielnachun <daniel.nachun@gmail.com>
2022-11-14 15:12:20 -0800

    Add create-dirs

--- .setup_env.sh
+++ .setup_env.sh
@@ -1,2 +1,2 @@
-curl -fLo $HOME/.local/bin/yadm https://github.com/TheLocehiliosan/yadm/raw/master/yadm && chmod a+x $HOME/.local/bin/yadm
-yadm clone --bootstrap https://github.com/danielnachun/yadm
+curl -fLo $HOME/.local/bin/yadm --create-dirs https://github.com/TheLocehiliosan/yadm/raw/master/yadm && chmod a+x $HOME/.local/bin/yadm
+$HOME/.local/bin/yadm clone --bootstrap https://github.com/danielnachun/yadm
